39e3e47052b0a0ff1e97f4f778573f7dc1d8ee2f
drblix <drblix75@gmail.com>
2022-03-08 14:04:24 -0500

    WIP

--- The Backrooms/Assets/Scripts/InventoryManager.cs
+++ The Backrooms/Assets/Scripts/InventoryManager.cs
@@ -18,13 +18,28 @@ public class InventoryManager : MonoBehaviour
     private Image slot2Icon;
     private Image slot3Icon;
 
+    [Header("Colors")]
+
     [SerializeField]
     private Color selectionColor;
     [SerializeField]
     private Color defaultColor;
 
+    private enum Slots
+    {
+        Slot1,
+        Slot2,
+        Slot3
+    }
+
+    [Header("Config")]
+
     [SerializeField]
-    private List<Item> inventory = new List<Item>();
+    private Slots selectedSlot;
+
+    [SerializeField]
+    private List<Item> inventory = new List<Item>(3);
+    // 0 = Slot 1; 1 = Slot 2; 2 = Slot 3
 
     private void Awake()
     {
@@ -32,10 +47,20 @@ public class InventoryManager : MonoBehaviour
         slot2Icon = slot2.GetChild(0).GetComponent<Image>();
         slot3Icon = slot3.GetChild(0).GetComponent<Image>();
 
+        slot1Icon.color = new Color(255f, 255f, 255f, 0f);
+        slot2Icon.color = new Color(255f, 255f, 255f, 0f);
+        slot3Icon.color = new Color(255f, 255f, 255f, 0f);
+
         slot1.GetComponent<Image>().color = selectionColor;
+        selectedSlot = Slots.Slot1;
     }
 
     private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
@@ -51,6 +76,45 @@ public class InventoryManager : MonoBehaviour
         {
             UpdateSelection("Slot3");
         }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Use item");
+        }
+    }
+
+    public void PickupItem(GameObject item)
+    {
+        Debug.Log(item.name);
+
+        if (inventory.Count < inventory.Capacity)
+        {
+            Debug.Log(inventory.Count);
+
+            switch (selectedSlot)
+            {
+                case Slots.Slot1:
+                    if (!inventory[1])
+                    {
+                        inventory.Insert(1, item.GetComponent<Item>());
+                    }
+                    break;
+
+                case Slots.Slot2:
+                    if (!inventory[2])
+                    {
+                        inventory.Insert(2, item.GetComponent<Item>());
+                    }
+                    break;
+
+                case Slots.Slot3:
+                    if (!inventory[3])
+                    {
+                        inventory.Insert(3, item.GetComponent<Item>());
+                    }
+                    break;
+            }
+        }
     }
 
     private void UpdateSelection(string slot)
@@ -58,18 +122,21 @@ public class InventoryManager : MonoBehaviour
         switch (slot)
         {
             case "Slot1":
+                selectedSlot = Slots.Slot1;
                 slot1.GetComponent<Image>().color = selectionColor;
                 slot2.GetComponent<Image>().color = defaultColor;
                 slot3.GetComponent<Image>().color = defaultColor;
                 break;
 
             case "Slot2":
+                selectedSlot = Slots.Slot2;
                 slot1.GetComponent<Image>().color = defaultColor;
                 slot2.GetComponent<Image>().color = selectionColor;
                 slot3.GetComponent<Image>().color = defaultColor;
                 break;
 
             case "Slot3":
+                selectedSlot = Slots.Slot3;
                 slot1.GetComponent<Image>().color = defaultColor;
                 slot2.GetComponent<Image>().color = defaultColor;
                 slot3.GetComponent<Image>().color = selectionColor;
--- The Backrooms/Assets/Scripts/PlayerCamera.cs
+++ The Backrooms/Assets/Scripts/PlayerCamera.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     private UIManager uiManager;
+    private InventoryManager invManager;
 
     [SerializeField] [Range(50f, 500f)]
     private float mouseSensitivity = 100f;
 
     private Transform plrCamera;
+
     [SerializeField]
     private Transform eventCam;
 
@@ -15,11 +17,19 @@ public class PlayerCamera : MonoBehaviour
 
     public static bool cameraInverted = false;
 
+    [Header("Raycast Config")]
+
+    [SerializeField] [Min(1f)]
+    private float rayDistance;
+
+    private int itemsLayer;
 
     private void Awake()
     {
         uiManager = FindObjectOfType<UIManager>();
+        invManager = FindObjectOfType<InventoryManager>();
         plrCamera = transform.Find("Head").Find("MainCamera");
+        itemsLayer = LayerMask.GetMask("Items");
 
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -29,6 +39,7 @@ public class PlayerCamera : MonoBehaviour
         if (uiManager.GameLoaded)
         {
             RotateCamera();
+            Raycast();
         }
     }
 
@@ -52,6 +63,23 @@ public class PlayerCamera : MonoBehaviour
         eventCam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void Raycast()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(plrCamera.position, plrCamera.TransformDirection(Vector3.forward));
+
+            Debug.DrawRay(plrCamera.position, plrCamera.TransformDirection(Vector3.forward), Color.red, 1f);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, rayDistance, itemsLayer))
+            {
+                GameObject obj = hitInfo.collider.gameObject;
+
+                invManager.PickupItem(obj);
+            }
+        }
     }
 
     public void ToggleInversion(bool state)
